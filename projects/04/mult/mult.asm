0e711ab87f17559664abd45d630360907eba3fb1
S.H <h3shiri@gmail.com>
2016-12-03 15:36:19 +0200

    Few basic modifications and adding the mult chip

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2 // This address will accumulate the result
+M = 0 
+@R0 // Stores RAM[1] inside D so we can use that number as a counter
+D = M 
+@COUNTER
+M = D
+
+(LOOP) // This loop will accumulate R1 to RESULT R0 Times
+@COUNTER
+D = M
+@END
+D;JEQ // If counter is 0 then we can go to end
+@R1
+D = M
+@R2
+M = M + D
+@COUNTER
+M = M - 1
+@LOOP
+0;JMP
+
+(END)
+0;JMP
