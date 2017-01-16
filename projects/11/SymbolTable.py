f36d2976e81b729c5fe3cf077a87d30d3fcbf7bb
S.H <h3shiri@gmail.com>
2017-01-16 23:32:59 +0200

    Assembling the symbol table

--- projects/11/SymbolTable.py
+++ projects/11/SymbolTable.py
@@ -5,34 +5,109 @@ class SymbolTable:
 	"""
 	This class represents an aymbol or the equivalent of a given node.
 	where each node is a given 
-	All tree traversing should be done with such objects as nodes.
+	All tree traversing should be done with such objects as nodes. 
 	"""
-
-	def __init__(self, fatherScope = None):
-		# We have 
+	TYPE = 0
+	KIND = 1
+	INDEX = 2
+	def __init__(self, fatherScope = None, scopeType = None):
+		# Here we have a ref to the father scope.
 		self.fatherScope = fatherScope
+		# Should be set on "class" or "subroutine".
+		self.scopeType = scopeType 
 		self.staticCounter = 0
 		self.fieldCounter = 0
 		self.argumentCounter = 0
 		self.varCounter = 0
-		# Each element has a label and points to an array of flags and values.
+		# A dictionary, the keys are the labels names and they point to various flags.
+		# name -> [Type, Kind, index]
 		self.elements = dict()
+		# offset dictionary
+		self.offsets = {"static":0, "field":0, "argument":0, "var":0}
+		
+	# Addidng a label to the current scope.	 
+	def addLabel(self, kind, type, name):
+		classKinds = {"static", "field"}
+		subroutineKinds = {"argument", "var"}
+		# If we try to define an already existing label
+		if name in self.getLocalLabels():
+			pass
+			#TODO: possible error overwriting an exisiting label.
+		if kind not in (classKinds | subroutineKinds):
+			pass
+			#TODO: error non existing type.
+		if ((kind in classKinds and self.scopeType != "class") or
+		   (kind in subroutineKinds and self.scopeType != "subroutine")) :
+			pass
+			# TODO: non appropriate scope error.
 
-	# returns all the local elements.
+		offset = self.VarCount(kind)
+		#Updating the ofset value in this scope
+		self.offsets[kind] += 1
+		# Inserting the relevant values in our table.
+		self.elements[name] = [type, kind, offset]
+	
+	# Returning how many of this kind we have already defined
+	def VarCount(self, kind):
+		return self.offsets[kind]
+	
+	# Returns relevent kind, if label exists in this Scope, else returns None.
+	def KindOf(self, name):
+		return self.localAttributeGetter(name, KIND)
+
+	# Returns relevent type, if label exists in this Scope, else returns None.
+	def TypeOf(self, name):
+		return self.localAttributeGetter(name, TYPE)
+
+	# Returns relevent index, if label exists in this Scope, else returns None.
+	def IndexOf(self, name):
+		return self.localAttributeGetter(name, INDEX)
+
+	# A utility function for all the getter attributes functions (TypeOf, IndexOf, KindOf)
+	def localAttributeGetter(self, name, attrIndex):
+		if name not in self.getLocalLabels():
+			return None
+		else:
+			attributes = self.elements[name]
+			return attributes[attrIndex]
+
+	# returns all the local labels.
 	def getLocalLabels(self):
 		return self.elements.keys()
 
 	"""
 	This is an important function fetching a target label,
 	in case we don't find it in the local scope we shall start traversing.
+	@return : relevant element attributes, return None if we failed to find it (plus error). 
 	"""
-	def getElement(self, target):
-		pass
+	def getElementAttributes(self, target):
+		if target in self.getLocalLabels():
+			return self.elements[target]
+		if self.hasFather():
+			#We should oly be able to climb this tree once.
+			return self.getFather().getElementAttributes(target)
+		else:
+			print("Non existing element")
+			return None
+
 
 	# Checks whether this node has a parent, useful in all recursions.
-	def hasParent(self):
+	def hasFather(self):
 		if self.fatherScope != None:
 			return True
+		else:
+			return False
+
+	#Getter function for father scope
+	def getFather(self):
+		return self.fatherScope
+
+	# Setting the father scope, note that we actually have only 2 scopes Class/Subroutine.
+	def setFather(self, fatherScope):
+		self.fatherScope = fatherScope
+
+
+
 
 #TODO: check the scope limitation is it actually just class and subroutine scopes (2)
 #which means no internal declarations this makes our ordeal significantly simpler??
@@ -41,12 +116,14 @@ class Tree:
 	This class represent our parsing tree.
 	Each Node is a symbol Table, and has internal methods for
 	fetching variables along the tree.
-	The leaves in this tree represent various terminals.
+	root scope should be class element, and respective leaves should be subroutines scopes.
 	"""
 	def __init__(self, xmlFile):
 		self.tree = ET.parse(xmlFile)
-		self.root = tree.getroot() # actually a fancy dict.
-		self.treeTableRoot = SymbolTable()
+		self.root = tree.getroot() # actually a fancy dict, derived from the xml file.
+
+		self.classTableRoot = SymbolTable(None, "class") # Class Scope Node
+		self.subroutineScopes = [] # fill this up as you parse the file.
 
 
 	"""from here on is the book API suggestion for symbol table"""
@@ -63,10 +140,4 @@ class Tree:
 	Arg and Var have a subroutine scope.
 	This should make our life simple.
 	"""
-	def Define(self, name, type, kind,):
-		pass
-
-	def VarCount(self, ):
-		pass
-
-	def KindOf
+	
